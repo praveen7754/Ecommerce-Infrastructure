fbd7ac82fbfb7fc014bcfdc23e38957a30e2d55d
Praveen <praveenkumar.dev25@gmail.com>
2026-08-20 20:29:03 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
-    # Replace this with the exact S3 bucket created for Terraform state.
-    bucket       = "REPLACE_WITH_TERRAFORM_STATE_BUCKET"
+    bucket       = "praveen-ecommerce-terraform-state-743320495203"
     key          = "ecommerce/terraform.tfstate"
     region       = "us-east-1"
     encrypt      = true
